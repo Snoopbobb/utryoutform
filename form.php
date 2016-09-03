bbf7d059b1f5595d98c7565829f8756f8167e363
Snoopbobb <bobtabor@q.com>
2016-09-03 12:41:16 -0700

    merge conflict

--- form.php
+++ form.php
@@ -1,236 +1,117 @@
-<<<<<<< HEAD
 <h1 class="cover-heading">Welcome to Utryout.com!</h1>
-	            		<p class="lead">A site to search and post tryout information for youth sports.</p>
-	            		<p class="lead">Launching soon!</p>
-	            		<div class="mastfoot">
-		            		<div class="inner">
-		              			<p>Please take a moment to fill out the form. We will never share your information.</p>
-		              			<p>By providing this information, it will help us develop a better site for you to use.</p>
-		            		</div>
-		          		</div>	
-	            		<form action="" method="POST" role="form">
-							<div class="form-group">
-								<!-- <label for="form-name">Name:</label> -->
-								<input type="text" name="name" id="form-name" class="form-control" placeholder="Name">
-							</div>
-							<div class="form-group">	
-								<!-- <label for="form-email">Email:</label> -->
-								<input type="email" name="email" id="form-email" class="form-control" placeholder="Email" required>
-							</div>
-							<div class="form-group">	
-								<!-- <label for="form-city">City:</label> -->
-								<input type="text" name="city" id="form-city" class="form-control" placeholder="City">
-							</div>
-							<!-- Hidden field for bots -->
-							<div class="pooh">	
-								<!-- <label for="form-city">City:</label> -->
-								<input type="text" name="phone" id="form-city" class="form-control" placeholder="Phone">
-							</div>
-							<div class="form-group">	
-								<!-- <label for="form-state">State:</label> -->
-								<select id="form-state" class="form-control" name="state">
-									<option value="" selected disabled>State</option>
-									<option value="AL">Alabama</option>
-									<option value="AK">Alaska</option>
-									<option value="AZ">Arizona</option>
-									<option value="AR">Arkansas</option>
-									<option value="CA">California</option>
-									<option value="CO">Colorado</option>
-									<option value="CT">Connecticut</option>
-									<option value="DE">Delaware</option>
-									<option value="DC">District Of Columbia</option>
-									<option value="FL">Florida</option>
-									<option value="GA">Georgia</option>
-									<option value="HI">Hawaii</option>
-									<option value="ID">Idaho</option>
-									<option value="IL">Illinois</option>
-									<option value="IN">Indiana</option>
-									<option value="IA">Iowa</option>
-									<option value="KS">Kansas</option>
-									<option value="KY">Kentucky</option>
-									<option value="LA">Louisiana</option>
-									<option value="ME">Maine</option>
-									<option value="MD">Maryland</option>
-									<option value="MA">Massachusetts</option>
-									<option value="MI">Michigan</option>
-									<option value="MN">Minnesota</option>
-									<option value="MS">Mississippi</option>
-									<option value="MO">Missouri</option>
-									<option value="MT">Montana</option>
-									<option value="NE">Nebraska</option>
-									<option value="NV">Nevada</option>
-									<option value="NH">New Hampshire</option>
-									<option value="NJ">New Jersey</option>
-									<option value="NM">New Mexico</option>
-									<option value="NY">New York</option>
-									<option value="NC">North Carolina</option>
-									<option value="ND">North Dakota</option>
-									<option value="OH">Ohio</option>
-									<option value="OK">Oklahoma</option>
-									<option value="OR">Oregon</option>
-									<option value="PA">Pennsylvania</option>
-									<option value="RI">Rhode Island</option>
-									<option value="SC">South Carolina</option>
-									<option value="SD">South Dakota</option>
-									<option value="TN">Tennessee</option>
-									<option value="TX">Texas</option>
-									<option value="UT">Utah</option>
-									<option value="VT">Vermont</option>
-									<option value="VA">Virginia</option>
-									<option value="WA">Washington</option>
-									<option value="WV">West Virginia</option>
-									<option value="WI">Wisconsin</option>
-									<option value="WY">Wyoming</option>
-								</select>
-							</div>
-							<h4>Which youth sport(s) would you like to see featured on this site?</h4>
-							<div class="btn-group">
-								<label for="form-baseball" class="btn btn-primary unchecked">Baseball</label>
-								<input type="checkbox" name="sport[]" value="baseball" id="form-baseball" class="hidden">
-								<label for="form-basketball" class="btn btn-primary unchecked">Basketball</label>
-								<input type="checkbox" name="sport[]" value="basketball" id="form-basketball" class="hidden">
-								<label for="form-football" class="btn btn-primary unchecked">Football</label>
-								<input type="checkbox" name="sport[]" value="football" id="form-football" class="hidden">								
-								<label for="form-hockey" class="btn btn-primary unchecked">Hockey</label>
-								<input type="checkbox" name="sport[]" value="hockey" id="form-hockey" class="hidden">								
-								<label for="form-soccer" class="btn btn-primary unchecked">Soccer</label>
-								<input type="checkbox" name="sport[]" value="soccer" id="form-soccer" class="hidden">
-							</div>
-							<div class="form-group">
-								<!-- <label for="form-other">Other Sports</label> -->
-								<input type="text" name="sport[]" id="form-other" placeholder="Other Sports">
-							</div>
-							<div class="form-group">
-								<h4>Would you like to be updated when this site is launched?</h4>
-								<label for="form-launch-yes" class="btn btn-primary launch-yes">Yes</label>
-								<input type="radio" name="site_launch" value="1" id="form-launch-yes" class="hidden">
-								<label for="form-launch-no" class="btn btn-primary launch-no">No</label>
-								<input type="radio" name="site_launch" value="0" id="form-launch-no" class="hidden">
-							</div>
-							<div class="form-group">
-								<h4>Would you like to be updated about future news or updates regarding this site?</h4>	
-								<label for="form-updates-yes" class="btn btn-primary updates-yes">Yes</label>
-								<input type="radio" name="site_updates" value="1" id="form-updates-yes" class="hidden">
-								<label for="form-updates-no" class="btn btn-primary updates-no">No</label>
-								<input type="radio" name="site_updates" value="0" id="form-updates-no" class="hidden">
-							</div>	
-							<button class="btn btn-lg btn-default">Submit</button>
-=======
-<h1 class="cover-heading">Welcome to Utryout.com!</h1>
-	            		<p class="lead">A site to search and post tryout information for youth sports.</p>
-	            		<p class="lead">Launching soon!</p>
-	            		<div class="mastfoot">
-		            		<div class="inner">
-		              			<p>Please take a moment to fill out the form. We will never share your information.</p>
-		              			<p>By providing this information, it will help us develop a better site for you to use.</p>
-		            		</div>
-		          		</div>	
-	            		<form action="" method="POST" role="form">
-							<div class="form-group">
-								<!-- <label for="form-name">Name:</label> -->
-								<input type="text" name="name" id="form-name" class="form-control" placeholder="Name">
-							</div>
-							<div class="form-group">	
-								<!-- <label for="form-email">Email:</label> -->
-								<input type="email" name="email" id="form-email" class="form-control" placeholder="Email" required>
-							</div>
-							<div class="form-group">	
-								<!-- <label for="form-city">City:</label> -->
-								<input type="text" name="city" id="form-city" class="form-control" placeholder="City">
-							</div>
-							<!-- Hidden field for bots -->
-							<div class="pooh">	
-								<!-- <label for="form-city">City:</label> -->
-								<input type="text" name="phone" id="form-city" class="form-control" placeholder="Phone">
-							</div>
-							<div class="form-group">	
-								<!-- <label for="form-state">State:</label> -->
-								<select id="form-state" class="form-control" name="state">
-									<option value="" selected disabled>State</option>
-									<option value="AL">Alabama</option>
-									<option value="AK">Alaska</option>
-									<option value="AZ">Arizona</option>
-									<option value="AR">Arkansas</option>
-									<option value="CA">California</option>
-									<option value="CO">Colorado</option>
-									<option value="CT">Connecticut</option>
-									<option value="DE">Delaware</option>
-									<option value="DC">District Of Columbia</option>
-									<option value="FL">Florida</option>
-									<option value="GA">Georgia</option>
-									<option value="HI">Hawaii</option>
-									<option value="ID">Idaho</option>
-									<option value="IL">Illinois</option>
-									<option value="IN">Indiana</option>
-									<option value="IA">Iowa</option>
-									<option value="KS">Kansas</option>
-									<option value="KY">Kentucky</option>
-									<option value="LA">Louisiana</option>
-									<option value="ME">Maine</option>
-									<option value="MD">Maryland</option>
-									<option value="MA">Massachusetts</option>
-									<option value="MI">Michigan</option>
-									<option value="MN">Minnesota</option>
-									<option value="MS">Mississippi</option>
-									<option value="MO">Missouri</option>
-									<option value="MT">Montana</option>
-									<option value="NE">Nebraska</option>
-									<option value="NV">Nevada</option>
-									<option value="NH">New Hampshire</option>
-									<option value="NJ">New Jersey</option>
-									<option value="NM">New Mexico</option>
-									<option value="NY">New York</option>
-									<option value="NC">North Carolina</option>
-									<option value="ND">North Dakota</option>
-									<option value="OH">Ohio</option>
-									<option value="OK">Oklahoma</option>
-									<option value="OR">Oregon</option>
-									<option value="PA">Pennsylvania</option>
-									<option value="RI">Rhode Island</option>
-									<option value="SC">South Carolina</option>
-									<option value="SD">South Dakota</option>
-									<option value="TN">Tennessee</option>
-									<option value="TX">Texas</option>
-									<option value="UT">Utah</option>
-									<option value="VT">Vermont</option>
-									<option value="VA">Virginia</option>
-									<option value="WA">Washington</option>
-									<option value="WV">West Virginia</option>
-									<option value="WI">Wisconsin</option>
-									<option value="WY">Wyoming</option>
-								</select>
-							</div>
-							<h4>Which youth sport(s) would you like to see featured on this site?</h4>
-							<div class="btn-group">
-								<label for="form-baseball" class="btn btn-primary unchecked">Baseball</label>
-								<input type="checkbox" name="sport[]" value="baseball" id="form-baseball" class="hidden">
-								<label for="form-basketball" class="btn btn-primary unchecked">Basketball</label>
-								<input type="checkbox" name="sport[]" value="basketball" id="form-basketball" class="hidden">
-								<label for="form-football" class="btn btn-primary unchecked">Football</label>
-								<input type="checkbox" name="sport[]" value="football" id="form-football" class="hidden">								
-								<label for="form-hockey" class="btn btn-primary unchecked">Hockey</label>
-								<input type="checkbox" name="sport[]" value="hockey" id="form-hockey" class="hidden">								
-								<label for="form-soccer" class="btn btn-primary unchecked">Soccer</label>
-								<input type="checkbox" name="sport[]" value="soccer" id="form-soccer" class="hidden">
-							</div>
-							<div class="form-group">
-								<!-- <label for="form-other">Other Sports</label> -->
-								<input type="text" name="sport[]" id="form-other" placeholder="Other Sports">
-							</div>
-							<div class="form-group">
-								<h4>Would you like to be updated when this site is launched?</h4>
-								<label for="form-launch-yes" class="btn btn-primary launch-yes">Yes</label>
-								<input type="radio" name="site_launch" value="1" id="form-launch-yes" class="hidden">
-								<label for="form-launch-no" class="btn btn-primary launch-no">No</label>
-								<input type="radio" name="site_launch" value="0" id="form-launch-no" class="hidden">
-							</div>
-							<div class="form-group">
-								<h4>Would you like to be updated about future news or updates regarding this site?</h4>	
-								<label for="form-updates-yes" class="btn btn-primary updates-yes">Yes</label>
-								<input type="radio" name="site_updates" value="1" id="form-updates-yes" class="hidden">
-								<label for="form-updates-no" class="btn btn-primary updates-no">No</label>
-								<input type="radio" name="site_updates" value="0" id="form-updates-no" class="hidden">
-							</div>	
-							<button class="btn btn-lg btn-default">Submit</button>
->>>>>>> 98542174f11297244f0950f67204c40645ffc1ff
-						</form>              			
+<p class="lead">A site to search and post tryout information for youth sports.</p>
+<p class="lead">Launching soon!</p>
+<div class="mastfoot">
+	<div class="inner">
+			<p>Please take a moment to fill out the form. We will never share your information.</p>
+			<p>By providing this information, it will help us develop a better site for you to use.</p>
+	</div>
+	</div>	
+<form action="" method="POST" role="form">
+	<div class="form-group">
+		<!-- <label for="form-name">Name:</label> -->
+		<input type="text" name="name" id="form-name" class="form-control" placeholder="Name">
+	</div>
+	<div class="form-group">	
+		<!-- <label for="form-email">Email:</label> -->
+		<input type="email" name="email" id="form-email" class="form-control" placeholder="Email" required>
+	</div>
+	<div class="form-group">	
+		<!-- <label for="form-city">City:</label> -->
+		<input type="text" name="city" id="form-city" class="form-control" placeholder="City">
+	</div>
+	<!-- Hidden field for bots -->
+	<div class="pooh">	
+		<!-- <label for="form-city">City:</label> -->
+		<input type="text" name="phone" id="form-city" class="form-control" placeholder="Phone">
+	</div>
+	<div class="form-group">	
+		<!-- <label for="form-state">State:</label> -->
+		<select id="form-state" class="form-control" name="state">
+			<option value="" selected disabled>State</option>
+			<option value="AL">Alabama</option>
+			<option value="AK">Alaska</option>
+			<option value="AZ">Arizona</option>
+			<option value="AR">Arkansas</option>
+			<option value="CA">California</option>
+			<option value="CO">Colorado</option>
+			<option value="CT">Connecticut</option>
+			<option value="DE">Delaware</option>
+			<option value="DC">District Of Columbia</option>
+			<option value="FL">Florida</option>
+			<option value="GA">Georgia</option>
+			<option value="HI">Hawaii</option>
+			<option value="ID">Idaho</option>
+			<option value="IL">Illinois</option>
+			<option value="IN">Indiana</option>
+			<option value="IA">Iowa</option>
+			<option value="KS">Kansas</option>
+			<option value="KY">Kentucky</option>
+			<option value="LA">Louisiana</option>
+			<option value="ME">Maine</option>
+			<option value="MD">Maryland</option>
+			<option value="MA">Massachusetts</option>
+			<option value="MI">Michigan</option>
+			<option value="MN">Minnesota</option>
+			<option value="MS">Mississippi</option>
+			<option value="MO">Missouri</option>
+			<option value="MT">Montana</option>
+			<option value="NE">Nebraska</option>
+			<option value="NV">Nevada</option>
+			<option value="NH">New Hampshire</option>
+			<option value="NJ">New Jersey</option>
+			<option value="NM">New Mexico</option>
+			<option value="NY">New York</option>
+			<option value="NC">North Carolina</option>
+			<option value="ND">North Dakota</option>
+			<option value="OH">Ohio</option>
+			<option value="OK">Oklahoma</option>
+			<option value="OR">Oregon</option>
+			<option value="PA">Pennsylvania</option>
+			<option value="RI">Rhode Island</option>
+			<option value="SC">South Carolina</option>
+			<option value="SD">South Dakota</option>
+			<option value="TN">Tennessee</option>
+			<option value="TX">Texas</option>
+			<option value="UT">Utah</option>
+			<option value="VT">Vermont</option>
+			<option value="VA">Virginia</option>
+			<option value="WA">Washington</option>
+			<option value="WV">West Virginia</option>
+			<option value="WI">Wisconsin</option>
+			<option value="WY">Wyoming</option>
+		</select>
+	</div>
+	<h4>Which youth sport(s) would you like to see featured on this site?</h4>
+	<div class="btn-group">
+		<label for="form-baseball" class="btn btn-primary unchecked">Baseball</label>
+		<input type="checkbox" name="sport[]" value="baseball" id="form-baseball" class="hidden">
+		<label for="form-basketball" class="btn btn-primary unchecked">Basketball</label>
+		<input type="checkbox" name="sport[]" value="basketball" id="form-basketball" class="hidden">
+		<label for="form-football" class="btn btn-primary unchecked">Football</label>
+		<input type="checkbox" name="sport[]" value="football" id="form-football" class="hidden">								
+		<label for="form-hockey" class="btn btn-primary unchecked">Hockey</label>
+		<input type="checkbox" name="sport[]" value="hockey" id="form-hockey" class="hidden">								
+		<label for="form-soccer" class="btn btn-primary unchecked">Soccer</label>
+		<input type="checkbox" name="sport[]" value="soccer" id="form-soccer" class="hidden">
+	</div>
+	<div class="form-group">
+		<!-- <label for="form-other">Other Sports</label> -->
+		<input type="text" name="sport[]" id="form-other" placeholder="Other Sports">
+	</div>
+	<div class="form-group">
+		<h4>Would you like to be updated when this site is launched?</h4>
+		<label for="form-launch-yes" class="btn btn-primary launch-yes">Yes</label>
+		<input type="radio" name="site_launch" value="1" id="form-launch-yes" class="hidden">
+		<label for="form-launch-no" class="btn btn-primary launch-no">No</label>
+		<input type="radio" name="site_launch" value="0" id="form-launch-no" class="hidden">
+	</div>
+	<div class="form-group">
+		<h4>Would you like to be updated about future news or updates regarding this site?</h4>	
+		<label for="form-updates-yes" class="btn btn-primary updates-yes">Yes</label>
+		<input type="radio" name="site_updates" value="1" id="form-updates-yes" class="hidden">
+		<label for="form-updates-no" class="btn btn-primary updates-no">No</label>
+		<input type="radio" name="site_updates" value="0" id="form-updates-no" class="hidden">
+	</div>	
+	<button class="btn btn-lg btn-default">Submit</button>
+</form>              			
